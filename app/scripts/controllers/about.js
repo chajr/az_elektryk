0cef360449c18480a4044251d556710b20ded9dd
chajr <chajr@bluetree.pl>
2015-08-15 12:55:39 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name azElektrykApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the azElektrykApp
+ */
+angular.module('azElektrykApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name azElektrykApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the azElektrykApp
+ */
+angular.module('azElektrykApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
